97962929072009eaf0571fd5f3e08ed9b586d9ad
Pedro Andres Ayala Bermudez <pedro.ayala-b@mail.escuelaing.edu.co>
2026-08-02 20:09:16 -0500

    i create a lamda funtion and the Class resultado

---
+++ challenge2/Resultado.java
@@ -0,0 +1,14 @@
+package challenge2;
+
+public class Resultado {
+    private int minValue;
+    private int maxValue;
+    private int numberElements;
+
+    public Resultado(int max){
+        this.maxValue = max;
+
+    }
+
+    
+}
