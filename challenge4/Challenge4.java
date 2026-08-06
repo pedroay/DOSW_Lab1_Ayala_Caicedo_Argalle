badb22489e694913adcda3cc051862ff74bec1ea
Javier Dario Caicedo Gomez <javier.caicezo@gmail.com>
2026-08-05 22:54:30 -0500

    creacion del metodo que usa el hash map

---
+++ challenge4/Challenge4.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Challenge4 {
+    public static HashMap<String, Integer> maps(List<Map.Entry<String, Integer>> pares) {
+        Function<List<Map.Entry<String, Integer>>, HashMap<String, Integer>> organizar = (lista) ->
+                lista.stream()
+                .sorted(Map.Entry.comparingByKey()).map(entry -> entry)
+                        .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(valorExistente, valorNuevo) -> valorExistente,HashMap::new));
+        return organizar.apply(pares);
+    }
+}
