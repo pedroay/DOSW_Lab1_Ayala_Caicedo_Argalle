16ffaa535d2af5b6ed35874bd12c423228e728ff
Javier Dario Caicedo Gomez <javier.caicezo@gmail.com>
2026-08-05 22:02:17 -0500

    creacion del metodo trasformMessage y el main

--- challenge3/Challenge3.java
+++ challenge3/Challenge3.java
@@ -1,4 +1,17 @@
-package challenge3;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Challenge3 {
+    public Challenge3() {
+        System.out.println("Hello World");
+    }
+    public static String TransformMessage(String mensaje) {
+        Function<String, String> reversa =(str)->new StringBuilder(str).reverse().toString();
+        return reversa.apply(mensaje);
+    }
+    public static void main(String[] args) {
+        String mensajeOriginal="hola";
+        String resultado=TransformMesssage(mensajeOriginal);
+    }
 }
